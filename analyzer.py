Cypresslin/regression-testing-reviewer

commit a69c91b6ddf5b74c1927d02981b97b08a14cdd96

analyzer.py: specify loader in yaml loader
Specify the Loader in yaml.load() to fix warning messages:
  YAMLLoadWarning: calling yaml.load() without Loader=... is deprecated
  as the default Loader is unsafe.

Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

analyzer.py
@@ -8,7 +8,7 @@ import utils
 def testsuite_validator(fullname, arch, testcases):
     try:
         with open('db-testcases.yaml', 'r') as stream:
-            db = yaml.load(stream)
+            db = yaml.load(stream, Loader=yaml.FullLoader)
             expected = db['full-list']
     except FileNotFoundError:
         print("db-testcases.yaml does not exist")
@@ -53,7 +53,7 @@ def analyze_that(link, testname, distro):
     unused = {}
     try:
         with open(fn, 'r') as stream:
-            issues = yaml.load(stream)
+            issues = yaml.load(stream, Loader=yaml.FullLoader)
     except FileNotFoundError:
         print("database file {} does not exist".format(fn))
         raise SystemExit

commit 607502d7b0baa2e12dbb81de6a80033505a4cf2a

analyzer.py: use SafeLoader for yaml loader
Use yaml SafeLoader instead of FullLoader for compatibility.

Fixes: a69c91b6ddf5 ("analyzer.py: specify loader in yaml loader")
Signed-off-by: Po-Hsu Lin <po-hsu.lin@canonical.com>

analyzer.py
@@ -8,7 +8,7 @@ import utils
 def testsuite_validator(fullname, arch, testcases):
     try:
         with open('db-testcases.yaml', 'r') as stream:
-            db = yaml.load(stream, Loader=yaml.FullLoader)
+            db = yaml.load(stream, Loader=yaml.SafeLoader)
             expected = db['full-list']
     except FileNotFoundError:
         print("db-testcases.yaml does not exist")
@@ -53,7 +53,7 @@ def analyze_that(link, testname, distro):
     unused = {}
     try:
         with open(fn, 'r') as stream:
-            issues = yaml.load(stream, Loader=yaml.FullLoader)
+            issues = yaml.load(stream, Loader=yaml.SafeLoader)
     except FileNotFoundError:
         print("database file {} does not exist".format(fn))
         raise SystemExit